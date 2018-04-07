c41895461de897f53c747706f135ac43f9f2503c
Christoph Schwizer <christoph.schwizer@uzh.ch>
2018-04-07 13:19:35 +0200

    Added IndexDocument class. Still requires a simhash implementation.

---
+++ src/ch/uzh/ifi/seal/ase/cscc/index/IndexDocument.java
@@ -0,0 +1,43 @@
+package ch.uzh.ifi.seal.ase.cscc.index;
+
+import org.uncommons.maths.binary.BitString;
+
+import java.util.List;
+import java.util.UUID;
+
+public class IndexDocument {
+
+    private static String id;
+    private String methodCall;
+    private String type;
+    private List<String> lineContext;
+    private List<String> overallContext;
+    private BitString lineContextSimhash;
+    private BitString overallContextSimhash;
+
+    public IndexDocument(String methodCall, String type, List<String> lineContext, List<String> overallContext) {
+        this.id = UUID.randomUUID().toString();
+        this.methodCall = methodCall;
+        this.type = type;
+        this.lineContext = lineContext;
+        this.overallContext = overallContext;
+        this.lineContextSimhash = createSimhashFromStrings(lineContext);
+        this.overallContextSimhash = createSimhashFromStrings(overallContext);
+    }
+
+    private BitString createSimhashFromStrings(List<String> strings) {
+        String concatenatedString = concatenate(strings);
+        // TODO: use Jenkin hash function to create 64 bit simhash
+        // [26] M. S. Uddin, C. K. Roy, K. A. Schneider, and A. Hindle, “On the Effectiveness of Simhash for Detecting Near-Miss Clones in Large Scale Software Systems”, in Proc. WCRE, 2011, pp. 13-22.
+        // C implementation: https://github.com/vilda/shash
+        return null;
+    }
+
+    private String concatenate(List<String> strings) {
+        String concatenatedString = "";
+        for (String s : strings) {
+            concatenatedString += s;
+        }
+        return concatenatedString;
+    }
+}
