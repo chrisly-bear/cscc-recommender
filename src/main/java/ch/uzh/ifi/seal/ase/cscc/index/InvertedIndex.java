07fd3d3b8cf5587d8037976098f1780b440b456e
Christoph Schwizer <christoph.schwizer@uzh.ch>
2018-04-16 22:43:34 +0200

    Implemented 3.2.4: initialize inverted index from disk.

--- src/main/java/ch/uzh/ifi/seal/ase/cscc/index/InvertedIndex.java
+++ src/main/java/ch/uzh/ifi/seal/ase/cscc/index/InvertedIndex.java
@@ -55,8 +55,8 @@ public class InvertedIndex {
 
         // create directory for contexts
         String contextsDirPath = invertedIndexDirPath + "/IndexDocuments";
-        File contextDir = new File(contextsDirPath);
-        contextDir.mkdir();
+        File contextsDir = new File(contextsDirPath);
+        contextsDir.mkdir();
 
         // create directory for inverted index structures
         String invertedIndexStructuresDirPath = invertedIndexDirPath + "/InvertedIndexStructures";
@@ -119,9 +119,61 @@ public class InvertedIndex {
     }
 
     public void initializeFromDisk(String sourceDir) {
-        // TODO 3.2.4: initialize index from disk
-        // ...
+        // initialize contexts
+        String contextsDirPath = sourceDir + "/InvertedIndex/IndexDocuments";
+        Map<String, IndexDocument> docIdIndexDocumentMap = initializeContexts(contextsDirPath);
+        // initialize inverted index structures
+        String invertedIndexStructuresDirPath = sourceDir + "/InvertedIndex/InvertedIndexStructures";
+        initializeInvertedIndexStructures(docIdIndexDocumentMap, invertedIndexStructuresDirPath);
     }
+
+    /**
+     * @param pathToContexts Folder in which the contexts (serialized IndexDocument objects) are stored
+     * @return Map where key is the document ID and value is the IndexDocument object
+     */
+    private Map<String, IndexDocument> initializeContexts(String pathToContexts) {
+        Map<String, IndexDocument> docIdIndexDocumentMap = new HashMap<>();
+        File contextsDir = new File(pathToContexts);
+        FilenameFilter filenameFilter = (dir, name) -> name.endsWith(".ser");
+        for (File file : contextsDir.listFiles(filenameFilter)) {
+            IndexDocument doc = deserializeIndexDocument(file.getAbsolutePath());
+            docIdIndexDocumentMap.put(doc.getId(), doc);
+        }
+        return docIdIndexDocumentMap;
+    }
+
+    private IndexDocument deserializeIndexDocument(String pathToFile) {
+        IndexDocument doc = null;
+        try {
+            FileInputStream fileIn = new FileInputStream(pathToFile);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            doc = (IndexDocument) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return doc;
+    }
+
+    /**
+     * Creates InvertedIndexStructures from postings files stored in {@code pathToInvertedIndexStructures}
+     * and puts them into the main index (INDEX 1).
+     * @param docIdIndexDocumentMap Maps document IDs with IndexDocument objects.
+     * @param pathToInvertedIndexStructures Directory in which the InvertedIndexStructures are stored.
+     */
+    private void initializeInvertedIndexStructures(Map<String, IndexDocument> docIdIndexDocumentMap, String pathToInvertedIndexStructures) {
+        File invertedIndexStructuresDir = new File(pathToInvertedIndexStructures);
+        FileFilter directoryFilter = file -> file.isDirectory();
+        for (File typeDir : invertedIndexStructuresDir.listFiles(directoryFilter)) {
+            // each type will get its own inverted index structure
+            String type = typeDir.getName();
+            InvertedIndexStructure iis = new InvertedIndexStructure(type);
+            iis.initializeFromDisk(typeDir, docIdIndexDocumentMap);
+            this.typeIndex.put(type, iis);
+        }
+    }
+
 
     // INDEX 2
     // Context Terms - Contexts
@@ -186,6 +238,61 @@ public class InvertedIndex {
 
         private Set<IndexDocument> getIndexedDocs() {
             return this.indexedDocs;
+        }
+
+        /**
+         * Instantiates this InvertedIndexStructure instance from postings lists stored on disk.
+         * @param dirContainingPostingsLists Directory where postings files are stored. The directory must have
+         *                                   the name of the type this instance represents, e.g.
+         *                                   {@code javax.swing.JInternalFrame}. Within this directory, each file should
+         *                                   have the name of the index term (i.e. the method name) and contain a set
+         *                                   of newline separated document IDs of documents (contexts) which contain the
+         *                                   term.
+         * @param idIndexDocumentMap Maps document IDs to IndexDocument objects.
+         */
+        private void initializeFromDisk(File dirContainingPostingsLists, Map<String, IndexDocument> idIndexDocumentMap) {
+            if (!dirContainingPostingsLists.isDirectory()) {
+                return;
+            }
+            String type = dirContainingPostingsLists.getName();
+            this.representedType = type;
+//            System.out.println(this.representedType);
+            FileFilter fileFilter = (file) -> file.isFile();
+            for (File postingsFile : dirContainingPostingsLists.listFiles(fileFilter)) {
+                String methodName = postingsFile.getName();
+//                System.out.println("   " + methodName);
+                List<String> docIDs = getDocIDsFromPostingsFile(postingsFile);
+                List<IndexDocument> indexDocuments = convertDocIDsToIndexDocuments(idIndexDocumentMap, docIDs);
+                indexedDocs.addAll(indexDocuments);
+                index.put(methodName, indexDocuments);
+            }
+        }
+
+        private List<String> getDocIDsFromPostingsFile(File postingsFile) {
+            List<String> docIDs = new LinkedList<>();
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(postingsFile));
+                String id;
+                while ((id = reader.readLine()) != null) {
+                    docIDs.add(id);
+//                    System.out.println("      " + id);
+                }
+                reader.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return docIDs;
+        }
+
+        private List<IndexDocument> convertDocIDsToIndexDocuments(Map<String, IndexDocument> idIndexDocumentMap, List<String> docIDs) {
+            List<IndexDocument> postings_objects = new LinkedList<>();
+            for (String docID : docIDs) {
+                IndexDocument docToIndex = idIndexDocumentMap.get(docID);
+                postings_objects.add(docToIndex);
+            }
+            return postings_objects;
         }
 
     }
