5f7e4ea8b829400846fd7bf2f2fef4da9be63f69
Christoph Schwizer <christoph.schwizer@uzh.ch>
2018-05-31 14:12:07 +0200

    Updated ParallelizedInvertedIndex: - Each thread has its own database connection. - ParallelizedInvertedIndex does not extend AbstractInvertedIndex (has a different API, namely the isIndexed and the serializeIndexDocument methods which require database connection objects to be passed around in the parallel version). - Lock collisions from Lucene and SQLite are printed to console.

--- src/main/java/ch/uzh/ifi/seal/ase/cscc/index/ParallelizedInvertedIndex.java
+++ src/main/java/ch/uzh/ifi/seal/ase/cscc/index/ParallelizedInvertedIndex.java
@@ -1,8 +1,12 @@
 package ch.uzh.ifi.seal.ase.cscc.index;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.LockObtainFailedException;
@@ -11,16 +15,21 @@ import java.io.*;
 import java.sql.*;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
-public class ParallelizedInvertedIndex extends AbstractInvertedIndex {
+public class ParallelizedInvertedIndex implements IInvertedIndex {
 
     /*
       CLASS & INSTANCE VARIABLES
      */
+
+    // fields for indexing in Lucene index
+    private static final String DOC_ID_FIELD = "docID";
+    private static final String OVERALL_CONTEXT_FIELD = "overallContext";
 
     private static final String SQL_TABLE_NAME = "indexdocuments";
     private final Logger LOGGER = Logger.getLogger(DiskBasedInvertedIndex.class.getName());
@@ -32,9 +41,6 @@ public class ParallelizedInvertedIndex extends AbstractInvertedIndex {
 
     // directory where the Lucene index is persisted on disk
     private String indexRootDir;
-
-    // connection to SQLite database
-    private Connection dbConn;
 
     // true: we store IndexDocuments in SQLite database
     // false: we serialize IndexDocuments to disk as files with .ser ending
@@ -68,22 +74,24 @@ public class ParallelizedInvertedIndex extends AbstractInvertedIndex {
         createDirectoryIfNotExists(new File(indexRootDir));
         this.USE_SQLITE = useRelationalDatabase;
         if (USE_SQLITE) {
-            openSQLConnection();
+            try {
+                createDBSchemaIfNotExists(openSQLConnection());
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    private void openSQLConnection() {
+    private Connection openSQLConnection() {
         String sqlUrl = "jdbc:sqlite:" + indexRootDir + "/" + SERIALIZED_INDEX_DOCUMENTS_SQLITE_FILE_NAME;
+        Connection dbConn = null;
         try {
             dbConn = DriverManager.getConnection(sqlUrl);
-            if (dbConn != null) {
-                dbConn.setAutoCommit(false);
-                createDBSchemaIfNotExists(dbConn);
-            }
         } catch (SQLException e) {
             e.printStackTrace();
             System.exit(1); // exit on exception
         }
+        return dbConn;
     }
 
     private void createDBSchemaIfNotExists(Connection sqlConnection) throws SQLException {
@@ -103,16 +111,9 @@ public class ParallelizedInvertedIndex extends AbstractInvertedIndex {
     }
 
     /**
-     * Call this method when this instance is not used anymore. It closes the SQLite connection.
+     * Call this method when this instance is not used anymore. It closes the ExecuterService.
      */
     public void cleanUp() {
-        if (USE_SQLITE && dbConn != null) {
-            try {
-                dbConn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
         executorService.shutdown();
     }
 
@@ -126,13 +127,17 @@ public class ParallelizedInvertedIndex extends AbstractInvertedIndex {
             @Override
             public void run() {
                 try {
-                    if (ParallelizedInvertedIndex.this.isIndexed(doc)) {
+                    // open a new db connection for each index task
+                    Connection dbConn = openSQLConnection();
+                    dbConn.setAutoCommit(false);
+                    if (ParallelizedInvertedIndex.this.isIndexed(dbConn, doc)) {
                         // do not put identical documents in index twice
-//            System.out.println("doc " + doc.getId() + " is already indexed");
+//                        System.out.println("doc " + doc.getId() + " is already indexed");
+                        dbConn.close(); // doc is already in index, we're done here. close db connection.
                         return;
                     }
 
-                    serializeIndexDocument(doc);
+                    serializeIndexDocument(dbConn, doc);
 
                     Directory indexDirectory = ParallelizedInvertedIndex.this.getIndexDirectory(doc);
                     IndexWriterConfig config = new IndexWriterConfig();
@@ -152,49 +157,70 @@ public class ParallelizedInvertedIndex extends AbstractInvertedIndex {
                         w.close();
                     }
                 } catch (LockObtainFailedException lockObtainFailedException) {
+                    // there's a lock on the Lucene index
                     // Resubmit this task
                     executorService.execute(this);
+                    System.out.println("LockObtainFailedException encountered. Keeping task in queue. DocID = " + doc.getId());
                 } catch (IOException e) {
                     e.printStackTrace();
                     executorService.shutdown();
                     System.exit(1); // exit on IOException
+                } catch (SQLException e) {
+                    if (e.getErrorCode() == 5) { // SQLITE_BUSY, i.e. there's another transaction which has a lock
+                        // Resubmit this task
+                        executorService.execute(this);
+                        System.out.println("SQLITE_BUSY encountered. Keeping task in queue. DocID = " + doc.getId());
+                    } else {
+                        System.out.println("SQLITE ERROR CODE: " + e.getErrorCode());
+                        e.printStackTrace();
+                    }
                 }
             }
         });
+    }
+
+    void addDocToLuceneIndex(IndexWriter w, IndexDocument doc) throws IOException {
+        Document luceneDoc = new Document();
+        luceneDoc.add(new StringField(DOC_ID_FIELD, doc.getId(), Field.Store.YES));
+        // store all terms in the overall context as tokens in the index
+        // StringField: no tokenization
+        // TextField: tokenization
+        for (String term : doc.getOverallContext()) {
+            luceneDoc.add(new StringField(OVERALL_CONTEXT_FIELD, term, Field.Store.NO));
+        }
+//        w.addDocument(luceneDoc); // this will add duplicates to an existing index
+        w.updateDocument(new Term(DOC_ID_FIELD, doc.getId()), luceneDoc); // don't index docs with same docID twice
+    }
+
+    @Override
+    public Set<IndexDocument> search(IndexDocument doc) {
+        // TODO: implement search (you can probably just copy it from DiskBasedInvertedIndex)
+        LOGGER.severe("Search for ParallelizedInvertedIndex not implemented!");
+        return null;
     }
 
     /*
       AbstractInvertedIndex IMPLEMENTATIONS
      */
 
-    @Override
-    boolean isIndexed(IndexDocument doc) {
+    boolean isIndexed(Connection dbConn, IndexDocument doc) throws SQLException {
         if (USE_SQLITE) {
-            return isIndexedInDB(doc);
+            return isIndexedInDB(dbConn, doc);
         } else {
             return isIndexedAsFile(doc);
         }
     }
 
-    private boolean isIndexedInDB(IndexDocument doc) {
+    private boolean isIndexedInDB(Connection dbConn, IndexDocument doc) throws SQLException {
         String sqlSelect = "SELECT docid FROM " + this.SQL_TABLE_NAME + " WHERE docid=\"" + doc.getId() + "\"";
-        try {
-            dbConn.setAutoCommit(false);
-            Statement stmt = dbConn.createStatement();
-            ResultSet rs = stmt.executeQuery(sqlSelect);
-            try {
-                dbConn.commit();
-            } catch (Exception ignored) {
-            }
-            boolean hasItems = rs.isBeforeFirst();
-//            System.out.println(doc.getId() + " already indexed? " + hasItems);
-            rs.close();
-            stmt.close();
-            return hasItems;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
+        dbConn.setAutoCommit(false);
+        Statement stmt = dbConn.createStatement();
+        ResultSet rs = stmt.executeQuery(sqlSelect);
+        boolean hasItems = rs.isBeforeFirst();
+//        System.out.println(doc.getId() + " already indexed? " + hasItems);
+        rs.close();
+        stmt.close();
+        return hasItems;
     }
 
     private boolean isIndexedAsFile(IndexDocument doc) {
@@ -206,22 +232,16 @@ public class ParallelizedInvertedIndex extends AbstractInvertedIndex {
         return indexRootDir + "/" + SERIALIZED_INDEX_DOCUMENTS_DIR_NAME + "/" + docID + ".ser";
     }
 
-    @Override
-    void serializeIndexDocument(IndexDocument doc) throws IOException {
+    void serializeIndexDocument(Connection dbConn, IndexDocument doc) throws IOException, SQLException {
         if (USE_SQLITE) {
-            try {
-                serializeToSQLite(doc);
-            } catch (SQLException e) {
-                e.printStackTrace();
-                throw new IOException(e.getMessage()); // TODO: it's probably not best practise to turn an SQLException into an IOException
-            }
+            serializeToSQLite(dbConn, doc);
         } else {
             serializeToFile(doc);
         }
     }
 
-    private void serializeToSQLite(IndexDocument doc) throws SQLException {
-        dbConn.setAutoCommit(false);
+    private void serializeToSQLite(Connection dbConn, IndexDocument doc) throws SQLException {
+//        dbConn.setAutoCommit(false); // we've already turned off autocommit in the 'isIndexedInDB', i.e. at this point we still have an open transaction
         String sqlInsert = "INSERT INTO " + this.SQL_TABLE_NAME + " VALUES(?,?,?,?,?,?,?)";
         PreparedStatement prepStmt = dbConn.prepareStatement(sqlInsert);
         prepStmt.setString(1, doc.getId());
@@ -231,11 +251,10 @@ public class ParallelizedInvertedIndex extends AbstractInvertedIndex {
         prepStmt.setString(5, serializeContext(doc.getOverallContext()));
         prepStmt.setLong(6, doc.getLineContextSimhash());
         prepStmt.setLong(7, doc.getOverallContextSimhash());
-        try {
-            dbConn.commit();
-        } catch (Exception ignored) {
-        }
+        prepStmt.executeUpdate();
+        dbConn.commit(); // end the transaction that we've started in the 'isIndexedInDB' call
         prepStmt.close();
+        dbConn.close(); // if we are at this point there has not been any exception which means the transaction was successful. we can close the db connection.
     }
 
     private void serializeToFile(IndexDocument doc) throws IOException {
@@ -248,7 +267,6 @@ public class ParallelizedInvertedIndex extends AbstractInvertedIndex {
         fileOut.close();
     }
 
-    @Override
     Directory getIndexDirectory(IndexDocument doc) throws IOException {
         String docType = doc.getType();
         String luceneIndexDirPath = indexRootDir + "/" + INVERTED_INDEX_STRUCTURES_DIR_NAME + "/" + docType;
@@ -256,7 +274,6 @@ public class ParallelizedInvertedIndex extends AbstractInvertedIndex {
         return fileDirectory;
     }
 
-    @Override
     IndexDocument deserializeIndexDocument(String docID) throws IOException {
         if (USE_SQLITE) {
             return deserializeFromSQLite(docID);
@@ -268,6 +285,7 @@ public class ParallelizedInvertedIndex extends AbstractInvertedIndex {
     private IndexDocument deserializeFromSQLite(String docID) {
         String sqlSelect = "SELECT * FROM " + this.SQL_TABLE_NAME + " WHERE docid=\"" + docID + "\"";
         try {
+            Connection dbConn = openSQLConnection();
             Statement stmt = dbConn.createStatement();
             ResultSet rs = stmt.executeQuery(sqlSelect);
             boolean hasItems = rs.isBeforeFirst();
@@ -283,6 +301,7 @@ public class ParallelizedInvertedIndex extends AbstractInvertedIndex {
             }
             rs.close();
             stmt.close();
+            dbConn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
