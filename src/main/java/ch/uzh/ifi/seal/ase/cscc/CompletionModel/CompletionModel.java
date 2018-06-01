699710eb6b76ef11f021c60faed060aa1e13849f
Tim Strasser <tim.strasser@modum.io>
2018-06-01 14:36:00 +0200

    Add javadoc in completion model

--- src/main/java/ch/uzh/ifi/seal/ase/cscc/CompletionModel/CompletionModel.java
+++ src/main/java/ch/uzh/ifi/seal/ase/cscc/CompletionModel/CompletionModel.java
@@ -9,14 +9,25 @@ import ch.uzh.ifi.seal.ase.cscc.visitors.IndexDocumentExtractionVisitorNoList;
 import java.io.IOException;
 import java.nio.file.*;
 
+/**
+ * Class representing the model trained by the datasets
+ */
 public class CompletionModel {
-    
+
     private IInvertedIndex index = null;
 
+    /**
+     * Creates a new completion model, representing the model trained by the datasets
+     * @param index An implementation of the IInvertedIndex interface
+     */
     public CompletionModel(IInvertedIndex index) {
         this.index = index;
     }
 
+    /**
+     * Takes a Context object from the KaVe datasets, creates a new IndexDocument and adds it to the model
+     * @param ctx The context object to add to the model
+     */
     public void train(Context ctx) {
         ISST sst = ctx.getSST();
 
@@ -26,11 +37,11 @@ public class CompletionModel {
     }
 
     /**
-     * Clean things up in the IInvertedIndex when we don't need the instance anymore.
+     * Call clean up methods on the index that was used to train the model
      */
     public void cleanUp() {
         if (index instanceof DiskBasedInvertedIndex) {
-            System.out.println("Closing DiskBasedInvertedIndex cleanly...");
+            System.out.println(String.format("Closing %s cleanly...", index.getClass().getName()));
             DiskBasedInvertedIndex dbindex = (DiskBasedInvertedIndex) index;
             dbindex.cleanUp();
         } else if (index instanceof ParallelizedInvertedIndex) {
@@ -46,6 +57,11 @@ public class CompletionModel {
         }
     }
 
+    /**
+     * Creates a new Recommender for the given IndexDocument, using the underlying model to recommend
+     * @param document
+     * @return A new recommender
+     */
     public Recommender getRecommender(IndexDocument document) {
         return new Recommender(index, document);
     }
