b4fb9bf26fc8e520a96ebb1f10a0167a75224a63
Tim Strasser <tim.strasser@modum.io>
2018-06-01 14:36:36 +0200

    fix capitalization

--- src/main/java/ch/uzh/ifi/seal/ase/cscc/utils/CSCCConfiguration.java
+++ src/main/java/ch/uzh/ifi/seal/ase/cscc/utils/CSCCConfiguration.java
@@ -2,22 +2,22 @@ package ch.uzh.ifi.seal.ase.cscc.utils;
 
 public class CSCCConfiguration {
 
-    // location of the KaVE dataset
+    // Location of the KaVE dataset
     public static final String EVENTS_DIR = "Data/Events";
     public static final String CONTEXTS_DIR = "Data/Contexts";
 
-    // location where the index should be stored
+    // Location where the index should be stored
     public static final String PERSISTENCE_LOCATION = "Data/Model";
     public static final String PERSISTENCE_LOCATION_TEST = "/tmp";
 
-    // number of statements to consider for overall context
+    // Number of statements to consider for overall context
     public static final int LAST_N_CONSIDERED_STATEMENTS = 6;
 
-    // print progress about which zip file is currently being processed
+    // Print progress about which zip file is currently being processed
     public static final boolean PRINT_PROGRESS = false;
-    // print progress about contexts being processed within a zip file
+    // Print progress about contexts being processed within a zip file
     public static final boolean PRINT_PROGRESS_CONTEXTS = true;
-    // limit the amount of training data, 0 (or smaller) for all data
+    // Limit the amount of training data, 0 (or smaller) for all data
     public static final int LIMIT_ZIPS = 20;
 
 
